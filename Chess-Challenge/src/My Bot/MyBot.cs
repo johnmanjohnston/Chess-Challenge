49e35d04ae38b4b8d03a82e152830682bb5fc5a3
johnmanjohnston <c.ethanjohn@gmail.com>
2023-07-26 18:03:49 +0400

    Implemented megamax algorithm

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,94 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
+    Move bestMove;
+    Board board;
+    Timer timer;
+
+    int searchDepth = 4;
+
+    int largeNumber = 9999999;
+    int[] pieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
+    bool isWhite = false;
+
+    public Move Think(Board _board, Timer _timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        board = _board;
+        timer = _timer;
+        isWhite = board.IsWhiteToMove;
+
+        NegamaxEvaluation(searchDepth, -largeNumber, largeNumber, isWhite ? 1 : -1);
+
+        return bestMove;
+    }
+
+    private int NegamaxEvaluation(int depth, int alpha, int beta, int color)
+    {
+        Move[] legalMoves = board.GetLegalMoves();
+
+        if (depth == 0 || legalMoves.Length == 0)
+        {
+            int sum = 0;
+
+            if (board.IsInCheckmate()) return -99999999;
+
+            for (int i = 0; ++i < 7;)
+            {
+                sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * pieceValues[i];
+            }
+
+            return color * sum;
+        }
+
+        int bestEvalution = int.MinValue;
+
+        foreach (Move move in legalMoves)
+        {
+            board.MakeMove(move);
+
+            if (board.IsDraw())
+            {
+                board.UndoMove(move);
+                continue;
+            }
+
+            int eval = -NegamaxEvaluation(depth - 1, -beta, -alpha, -color);
+
+            if (TotalPieceCount(color == 1) < 8 && move.MovePieceType == PieceType.Pawn)
+            {
+                // Console.WriteLine("Encouraging pawns to move forward");
+                eval += 200; // Encourage pawns to move forward
+            }
+
+            board.UndoMove(move);
+
+            if (bestEvalution < eval)
+            {
+                bestEvalution = eval;
+                if (depth == searchDepth)
+                {
+                    bestMove = move;
+                }
+            }
+
+            alpha = Math.Max(alpha, eval);
+            if (alpha >= beta) break;
+        }
+
+        return bestEvalution;
+    }
+
+    private int TotalPieceCount(bool white)
+    {
+        int count = 0;
+
+        for (int i = 0; ++i < 7;)
+        {
+            count += board.GetPieceList((PieceType)i, white).Count;
+        }
+
+        return count;
     }
 }
